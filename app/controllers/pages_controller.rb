ca6ad8bcfe756c76f52faf8fdbcc99dca2f8da2b
kjohnson0451 <kjohnson0451@gmail.com>
2016-09-11 03:28:11 +0000

    Completed task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,17 @@ class PagesController < ApplicationController
   end
 
   def sms
+    @account_sid = ENV['TWILIO_ACCOUNT_SID']
+    @auth_token = ENV['TWILIO_AUTH_TOKEN']
+    @from_number = ENV['FROM_NUMBER']
+    @message = params[:message]
+    @to_number = params[:user][:number]
 
+    @client = Twilio::REST::Client.new @account_sid, @auth_token
+    message = @client.account.messages.create(:body => @message,
+                                              :to => @to_number,   # Replace with your phone number
+                                              :from => @from_number)  # Replace with your Twilio number
+
+    puts message.sid
   end
 end
